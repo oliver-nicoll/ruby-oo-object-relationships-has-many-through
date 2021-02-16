831713bf6f455dc4ee2736f28f0bd3e99178527c
Nicoll Oliver <nicolldon96@gmail.com>
2021-02-16 17:58:44 -0500

    commit tree 480e5974c4b7bd2ce8d3d580b6e8260e8b60ac9f

--- lib/meal.rb
+++ lib/meal.rb
@@ -1,3 +1,17 @@
 class Meal
+    attr_accessor :waiter, :customer, :total, :tip
 
+    @@all = [] #class variable again(like in the customer and waiter class) here to collect all the instances into the array
+
+    def initialize(waiter, customer, total, tip=0) #has waiter and customer to tie in all three together
+        @waiter = waiter 
+        @customer = customer
+        @total = total
+        @tip = tip
+        @@all << self
+    end
+
+    def self.all #class method also in customer and waiter class
+        @@all
+    end
 end
--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,34 @@
 class Waiter
+    attr_accessor :name, :yrs_experience
+    @@all = [] #this @@all(class variable) array is also in the customer class
+
+    def initialize(name, yrs_experience)
+        @name = name
+        @yrs_experience = yrs_experience
+        @@all << self
+    end
+
+    def self.all    #this class method is also in the class customer
+        @@all
+    end
+
+    def new_meal(customer, total, tip=0) #adds a new meal instance, refers to waiter as self
+        Meal.new(self, customer, total, tip)
+    end
+
+    def meals
+        Meal.all.select do |meal|
+            meal.waiter == self #checking for waiter
+        end
+    end
+
+    def best_tipper
+        best_tipped_meal = meals.max do |meal_a, meal_b|
+            meal_a.tip <=> meal_b.tip
+        end
+        
+        best_tipped_meal.customer
+    end
+
 
 end
